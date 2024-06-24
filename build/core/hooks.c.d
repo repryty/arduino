91bf769350ed2778623119e8dfeb250d46c49b44
repryty <62415785+repryty@users.noreply.github.com>
2024-06-25 00:47:33 +0900

    Delete build directory

--- build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\LG\Documents\GitHub\arduino\build\core\hooks.c.o: \
- C:\Users\LG\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\hooks.c
--- build/core/new.cpp.d
+++
@@ -1,4 +0,0 @@
-c:\Users\LG\Documents\GitHub\arduino\build\core\new.cpp.o: \
- C:\Users\LG\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new.cpp \
- C:\Users\LG\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new.h \
- C:\Users\LG\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\new
